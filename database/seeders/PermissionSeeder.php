9025de63ac6087e8516dd7e6307958e84223d2fe
Rehvid <dharendarz2@gmail.com>
2022-11-17 20:26:09 +0100

    feat: update permission seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -23,6 +23,8 @@ class PermissionSeeder extends Seeder
         Permission::create(['name' => 'users.change_role']);
         Permission::create(['name' => 'users.assign_to_team']);
 
+        Permission::create(['name' => 'log-viewer']);
+
         $adminRole = Role::findByName(config('auth.roles.admin'));
         $adminRole->givePermissionTo('users.index');
         $adminRole->givePermissionTo('users.store');
@@ -31,5 +33,6 @@ class PermissionSeeder extends Seeder
         $adminRole->givePermissionTo('users.change_role');
         $adminRole->givePermissionTo('users.assign_to_team');
 
+        $adminRole->givePermissionTo('log-viewer');
     }
 }
