bfaab6918706e8e2bae54febb3a9a5a9a7494ca8
Rehvid <dharendarz2@gmail.com>
2022-12-01 22:27:06 +0100

    feat: create actions and filters for status table

---
+++ app/Http/Livewire/Statuses/Actions/RestoreStatusAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire\Statuses\Actions;
+
+use LaravelViews\Views\View;
+
+class RestoreStatusAction extends \LaravelViews\Actions\Action
+{
+    public $title = '';
+    public $icon = 'eye';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->title = 'Przywróć status';
+    }
+
+    public function handle($model, View $view)
+    {
+        $view->dialog()->confirm([
+            'title' => 'Przywracanie statusu',
+            'description' => 'Czy na pewno przywrócić status ' . $model->name,
+            'icon' => 'question',
+            'iconColor' => 'text-green-500',
+            'accept' => [
+                'label' => 'Tak',
+                'method' => 'restore',
+                'params' => $model->id
+            ],
+            'reject' => [
+                'label' => 'Nie'
+            ]
+        ]);
+    }
+
+    public function  renderIf($model, View $view)
+    {
+        return $model->deleted_at !== null;
+    }
+}
