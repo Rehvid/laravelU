08b6898a98b78aed13f6b3dfba1faaaf58d9d289
Rehvid <dharendarz2@gmail.com>
2022-12-10 20:03:05 +0100

    feat: update team table

--- app/Http/Livewire/Teams/TeamsTableView.php
+++ app/Http/Livewire/Teams/TeamsTableView.php
@@ -6,6 +6,7 @@ use App\Models\Team;
 use LaravelViews\Facades\Header;
 use LaravelViews\Views\TableView;
 use Illuminate\Contracts\Database\Eloquent\Builder;
+use App\Http\Livewire\Teams\Filters\SoftTeamDeleteFilter;
 
 class TeamsTableView extends TableView
 {
@@ -14,7 +15,15 @@ class TeamsTableView extends TableView
      */
     protected $model = Team::class;
 
-    protected $paginate = 5;
+    protected $paginate = 10;
+
+    public $searchBy = [
+        'name',
+        'user.name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 
     /**
      * Sets the headers of the table as you want to be displayed
@@ -25,7 +34,7 @@ class TeamsTableView extends TableView
     {
         return [
             Header::title('name')->sortBy('name'),
-            Header::title('user_id')->sortBy('user_id'),
+            Header::title('user')->sortBy('user'),
             Header::title('created_at')->sortBy('created_at'),
             Header::title('updated_at')->sortBy('updated_at'),
             Header::title('deleted_at')->sortBy('deleted_at'),
@@ -34,7 +43,7 @@ class TeamsTableView extends TableView
 
     public function repository(): Builder
     {
-        return Team::query()->withTrashed();
+        return Team::query()->with('user')->withTrashed();
     }
 
     /**
@@ -46,10 +55,17 @@ class TeamsTableView extends TableView
     {
         return [
             $model->name,
-            $model->user_id,
+            $model->user->name,
             $model->created_at,
             $model->updated_at,
             $model->deleted_at,
+        ];
+    }
+
+    protected function filters()
+    {
+        return [
+            new SoftTeamDeleteFilter()
         ];
     }
 }
