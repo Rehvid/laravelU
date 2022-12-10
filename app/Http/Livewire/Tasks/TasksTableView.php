25e4f6b651b6dea045e7387574bf17afd8eaa417
Rehvid <dharendarz2@gmail.com>
2022-12-10 20:02:00 +0100

    feat: update table for tasks

--- app/Http/Livewire/Tasks/TasksTableView.php
+++ app/Http/Livewire/Tasks/TasksTableView.php
@@ -2,30 +2,58 @@
 
 namespace App\Http\Livewire\Tasks;
 
+
 use App\Models\Task;
 use LaravelViews\Facades\Header;
 use LaravelViews\Views\TableView;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 
+
+use App\Http\Livewire\Tasks\Actions\EditTaskAction;
+use App\Http\Livewire\Tasks\Actions\RestoreTaskAction;
+use App\Http\Livewire\Tasks\Actions\SoftDeleteTaskAction;
+
+use App\Http\Livewire\Tasks\Filters\SoftTaskDeleteFilter;
+use WireUi\Traits\Actions;
+
+
+
 class TasksTableView extends TableView
 {
+
+    use Actions;
+
     /**
      * Sets a model class to get the initial data
      */
     protected $model = Task::class;
-    protected $paginate = 5;
+    protected $paginate = 10;
 
     /**
      * Sets the headers of the table as you want to be displayed
      *
      * @return array<string> Array of headers
      */
+
+
+    public $searchBy = [
+        'user.name',
+        'team.name',
+        'status.name',
+        'title',
+        'deadline',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
     public function headers(): array
     {
+
         return [
-            Header::title('user_id')->sortBy('user_id'),
-            Header::title('team_id')->sortBy('team_id'),
-            Header::title('status_id')->sortBy('status_id'),
+            Header::title('user')->sortBy('user'),
+            Header::title('team')->sortBy('team'),
+            Header::title('status')->sortBy('status'),
             Header::title('title')->sortBy('title'),
             Header::title('description')->sortBy('description'),
             Header::title('deadline')->sortBy('deadline'),
@@ -37,7 +65,7 @@ class TasksTableView extends TableView
 
     public function repository(): Builder
     {
-        return Task::query()->withTrashed();
+        return Task::query()->with('user', 'team', 'status')->withTrashed();
     }
 
     /**
@@ -48,9 +76,9 @@ class TasksTableView extends TableView
     public function row($model): array
     {
         return [
-            $model->user_id,
-            $model->team_id,
-            $model->status_id,
+            $model->user->name,
+            $model->team->name ?? '',
+            $model->status->name ?? '',
             $model->title,
             $model->description,
             $model->deadline,
@@ -59,4 +87,35 @@ class TasksTableView extends TableView
             $model->deleted_at
         ];
     }
+
+    protected function filters()
+    {
+        return [
+            new SoftTaskDeleteFilter()
+        ];
+    }
+
+    protected function actionsByRow()
+    {
+        return [
+            new EditTaskAction('task.edit', 'Edytuj'),
+            new RestoreTaskAction(),
+            new SoftDeleteTaskAction(),
+        ];
+    }
+
+    public function softDelete($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        $this->notification()->success('Usunięto', 'Usunięto zadanie ' . $task->title);
+    }
+
+    public function restore($id)
+    {
+        $task = Task::withTrashed()->find($id);
+        $task->restore();
+        $this->notification()->success('Przywrócono zadanie ' . $task->name);
+    }
+
 }
