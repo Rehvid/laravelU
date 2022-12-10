7ad59302fafff6b719f821a3160983ee37d02072
Rehvid <dharendarz2@gmail.com>
2022-12-10 16:57:14 +0100

    feat: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Team;
 use App\Models\User;
 use App\Models\Status;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 use Database\Seeders\TeamSeeder;
 use Spatie\Permission\Models\Permission;
@@ -24,9 +25,11 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(StatusSeeder::class);
         $this->call(TeamSeeder::class);
+        $this->call(TaskSeeder::class);
 
         User::factory(10)->create();
         Status::factory(10)->create();
         Team::factory(10)->create();
+        Task::factory(10)->create();
     }
 }
