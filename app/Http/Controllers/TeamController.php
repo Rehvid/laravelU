3bc3a469084a4366f4f797c50462faf55ce5e6bb
Rehvid <dharendarz2@gmail.com>
2022-12-27 10:23:24 +0100

    feat: create new route method also add strict type

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -1,22 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\Team;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
 
 class TeamController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         $this->authorize('viewAny', Team::class);
 
-        return view(
-            'team.index',
-            [
+        return view('team.index',[
                 'teams' => Team::withTrashed()->get(),
             ]
         );
+    }
+
+    public function create(): View
+    {
+        $this->authorize('create', Team::class);
+
+        return view('team.form');
     }
 
 }
