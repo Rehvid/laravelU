d4917147dda8f5cb81313e1e691a2ac6fa610365
Rehvid <dharendarz2@gmail.com>
2023-01-03 20:59:22 +0100

    feat: add policy for user

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+     /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->can('users.index');
+    }
+
+    public function assignTeam(User $user)
+    {
+        return $user->can('users.assign_to_team');
+    }
+
+}
